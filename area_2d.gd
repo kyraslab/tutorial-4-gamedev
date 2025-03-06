7eb8b9db9b7abc0a344a28964a880854dfccd054
kyraslab <alicia.kirana@ui.ac.id>
2025-03-06 15:16:32 +0700

    refactor: make folders to improve directory

--- area_2d.gd
+++ area_2d.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.get_name() == "Player":
-		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
+		get_tree().change_scene_to_file(str("res://scenes/screens/" + sceneName + ".tscn"))
